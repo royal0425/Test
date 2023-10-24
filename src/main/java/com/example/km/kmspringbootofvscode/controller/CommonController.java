906f01032006947fb4a0d7e768e3bb039263194b
josub-kim <rlawhtjq@gmail.com>
2023-10-24 16:27:45 +0900

    fix

--- src/main/java/com/example/km/kmspringbootofvscode/controller/CommonController.java
+++ src/main/java/com/example/km/kmspringbootofvscode/controller/CommonController.java
@@ -11,7 +11,7 @@ public class CommonController{
     @RequestMapping(value = "/")
     public String enter() {
         HashMap result = new HashMap();
-        result.put("message", "안녕하세요");
+        result.put("message", "안녕하세요 a             asdfafsadfsadfasdfasdfdasf");
 
         return "index";
     }
@@ -19,7 +19,7 @@ public class CommonController{
     @RequestMapping(value = "/api")
     public HashMap test() {
         HashMap result = new HashMap();
-        result.put("message", "안녕하세요.");
+        result.put("message", "안녕하세요.  aaaaaaaaaaaaaaaadasfsdafdsfadsfaaa ");
         System.out.println("Hello");
         return result;
     }
