d25c0ec8fe4232e3fe5455d11f964e9c1a391df7
tlagmlwns <simh4jun@gmail.com>
2023-10-24 16:54:10 +0900

    upstream test3

--- src/main/java/com/example/km/kmspringbootofvscode/controller/CommonController.java
+++ src/main/java/com/example/km/kmspringbootofvscode/controller/CommonController.java
@@ -19,15 +19,9 @@ public class CommonController{
     @RequestMapping(value = "/api")
     public HashMap test() {
         HashMap result = new HashMap();
-<<<<<<< HEAD
-<<<<<<< HEAD
         result.put("message", "하이방가루~");
-=======
         result.put("message", "반갑습니다.");
->>>>>>> 7d7e90974d9e989d21f90fe588ff682ba735d182
-=======
         result.put("message", "안녕하세요.  aaaaaaaaaaaaaaaadasfsdafdsfadsfaaa ");
->>>>>>> acd593f7178b8356dfb5434a46538548f15790f4
         System.out.println("Hello");
         return result;
     }
